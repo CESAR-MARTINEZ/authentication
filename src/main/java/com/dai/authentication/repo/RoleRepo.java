8645d4fe4844f1527e7eebcfa82eed90ca860b97
CESAR MARTINEZ <cesarfernando.11@gmail.com>
2022-02-15 18:43:50 -0500

    repo de Role

--- src/main/java/com/dai/authentication/repo/RoleRepo.java
+++ src/main/java/com/dai/authentication/repo/RoleRepo.java
@@ -1,5 +1,6 @@
 package com.dai.authentication.repo;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,5 @@ import com.dai.authentication.domain.Role;
 
 public interface RoleRepo extends JpaRepository<Role, Long> {
 	Optional<Role> findById(Long id);
-	
+	List<Role> findByNombre(String nombre);
 }
