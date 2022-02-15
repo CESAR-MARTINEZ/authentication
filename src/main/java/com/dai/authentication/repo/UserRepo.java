6e57c62337ba56d5e6860b0a84de9a0865661b77
CESAR MARTINEZ <cesarfernando.11@gmail.com>
2022-02-15 18:47:26 -0500

    Repo User

--- src/main/java/com/dai/authentication/repo/UserRepo.java
+++ src/main/java/com/dai/authentication/repo/UserRepo.java
@@ -1,9 +1,12 @@
 package com.dai.authentication.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dai.authentication.domain.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
-
+	List<User> findAll();
+	
 }
